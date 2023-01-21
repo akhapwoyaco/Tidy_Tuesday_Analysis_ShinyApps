f9d5721fba395ccf1b5976c837bd4239078f8c9e
Akhapwoya Obuchere <thechristopheao@gmail.com>
2023-01-21 18:42:32 +0300

    Up until June 2018 tidytuesday.

--- set_work_directory.R
+++ set_work_directory.R
@@ -1,4 +1,19 @@
 #
-setwd('2018/2018-04-30/')
+setwd('2018/2018-06-26')
 setwd('..')
 #
+
+
+# Get the Data
+
+# Read in with tidytuesdayR package 
+# Install from CRAN via: install.packages("tidytuesdayR")
+# This loads the readme and all the datasets for the week of interest
+
+# Either ISO-8601 date or year/week works!
+
+tuesdata <- tidytuesdayR::tt_load('2018-06-19')
+
+tuesdata <- tidytuesdayR::tt_load('2018-06-26')
+
+tuesdata$week13_alcohol_global
